c4f188189853b2c15c29aabfc9a7e76195058053
Flanker <167943046+superflanker27@users.noreply.github.com>
2026-08-21 05:22:42 +0200

    Update faggor.bat

--- faggor.bat
+++ faggor.bat
@@ -1,3 +1,3 @@
 @echo off
 powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/superflanker27/gotov/refs/heads/main/gotovenv.ps1' -OutFile 'C:\Users\WDAGUtilityAccount\Desktop\gotov.psi'"
-start powershell -NoExit -ExecutionPolicy Bypass -File "C:\Users\WDAGUtilityAccount\Desktop\gotov.psi"
+start powershell -NoExit -ExecutionPolicy Bypass -File "C:\Users\WDAGUtilityAccount\Desktop\gotov.ps1"
